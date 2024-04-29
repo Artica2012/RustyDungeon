86dacf6b2ed6c50a16452bc3ad6afd56a60edd26
Artica2012 <schneiaj@gmail.com>
2024-04-28 23:17:07 -0500

    Initializes Boardstate

---
+++ src/board/components.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+use bevy::prelude::{Component, Entity, Resource};
+use crate::vectors::Vector2Int;
+
+#[derive(Component)]
+pub struct Position{
+    pub v: Vector2Int
+}
+
+#[derive(Component)]
+pub struct Tile;
+
+#[derive(Default, Resource)]
+pub struct BoardRes {
+    pub tiles: HashMap<Vector2Int, Entity>
+}
---
+++ src/board/mod.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use bevy::app::App;
+use bevy::prelude::{Entity, OnEnter, Plugin, Resource};
+use crate::states::MainState;
+use crate::vectors::Vector2Int;
+
+pub mod components;
+mod systems;
+
+
+pub struct BoardPlugin;
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<CurrentBoard>()
+            .add_systems(OnEnter(MainState::Game), systems::spawn_map);
+    }
+}
+
+#[derive(Default, Resource)]
+pub struct CurrentBoard{
+    pub tiles: HashMap<Vector2Int, Entity>
+}
